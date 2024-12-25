d42e91c8d25fb9083e0b4a6f38626d8b4109c7a2
Neo <hungnguyen190203@gmail.com>
2024-12-25 15:36:59 +0700

    format_code

--- src/index.js
+++ src/index.js
@@ -1,27 +1,32 @@
 const express = require('express');
 const morgan = require('morgan');
-const {engine} = require('express-handlebars');
-const path = require('path'); 
+const { engine } = require('express-handlebars');
+const path = require('path');
 const app = express();
-const port = 3000; 
+const port = 3000;
+const route = require('./routes');
 
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+);
+app.use(express.json());
 
 //Http logger
 app.use(morgan('combined'));
 
 //Template Engine
 
-app.engine('.hbs', engine({extname: '.hbs'}));
+app.engine('.hbs', engine({ extname: '.hbs' }));
 app.set('view engine', '.hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
-
-
-app.get('/', (req, res) => {
-  res.render('home');
-})
+//Routes init
+route(app);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-})
+    console.log(`Example app listening on port ${port}`);
+});
